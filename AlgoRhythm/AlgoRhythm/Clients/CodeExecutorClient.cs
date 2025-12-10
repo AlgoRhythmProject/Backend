74ade017b0a0bfea89a254730c667a9e0e98e482
Ula <urszula.obirek@gmail.com>
2025-12-10 22:08:29 +0100

    Added tests

--- AlgoRhythm/AlgoRhythm/Clients/CodeExecutorClient.cs
+++ AlgoRhythm/AlgoRhythm/Clients/CodeExecutorClient.cs
@@ -9,12 +9,10 @@ namespace AlgoRhythm.Clients
     public class CodeExecutorClient
     {
         private readonly HttpClient _client;
-        private readonly ILogger<CodeExecutorClient> _logger;
 
-        public CodeExecutorClient(HttpClient client, ILogger<CodeExecutorClient> logger)
+        public CodeExecutorClient(HttpClient client)
         {
             _client = client;
-            _logger = logger;
         }
 
         public async Task<List<TestResultDto>?> ExecuteAsync(List<ExecuteCodeRequest> req)
@@ -26,47 +24,36 @@ namespace AlgoRhythm.Clients
                 ReferenceHandler = ReferenceHandler.IgnoreCycles
             };
 
-            // ======== LOG REQUEST ========
             try
             {
                 string json = JsonSerializer.Serialize(req, jsonOptions);
-                _logger.LogInformation("Sending request to JUDGE:\n" + json);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Failed to serialize ExecuteCodeRequest list for logging");
             }
-            // ==============================
 
             try
             {
                 HttpResponseMessage response =
                     await _client.PostAsJsonAsync("http://executor:8080/code-executor/Execute", req);
 
-                _logger.LogInformation("Judge responded with HTTP {StatusCode}", response.StatusCode);
 
                 response.EnsureSuccessStatusCode();
 
                 var result =
                     await response.Content.ReadFromJsonAsync<List<TestResultDto>>();
 
-                // ======== LOG RESPONSE ========
                 try
                 {
                     string jsonResult = JsonSerializer.Serialize(result, jsonOptions);
-                    _logger.LogInformation("Judge response JSON:\n{Json}", jsonResult);
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, "Failed to serialize TestResultDto list for logging");
                 }
-                // ===============================
-
                 return result;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error when communicating with judge service");
 
                 return
                 [
