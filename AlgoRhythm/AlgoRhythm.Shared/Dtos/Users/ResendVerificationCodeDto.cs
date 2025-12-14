ea1a2ca0b1b37d7fd2f683d83077fc0af3845c36
Wojciech Łapan <Wojtekl770@interia.pl>
2025-12-14 11:29:15 +0100

    zmiana hasla, forgot password, i ponowne wyslanie kodu weryfikacji

---
+++ AlgoRhythm/AlgoRhythm.Shared/Dtos/Users/ResendVerificationCodeDto.cs
@@ -0,0 +1,6 @@
+namespace AlgoRhythm.Shared.Dtos.Users;
+
+public class ResendVerificationCodeDto
+{
+    public required string Email { get; set; }
+}
--- AlgoRhythm/AlgoRhythm/Services/Users/Interfaces/IAuthService.cs
+++ AlgoRhythm/AlgoRhythm/Services/Users/Interfaces/IAuthService.cs
@@ -7,4 +7,8 @@ public interface IAuthService
     Task RegisterAsync(RegisterRequest request);
     Task<AuthResponse> VerifyEmailAsync(VerifyEmailRequest request);
     Task<AuthResponse> LoginAsync(LoginRequest request);
+    Task ResendVerificationCodeAsync(ResendVerificationCodeDto request);
+    Task RequestPasswordResetAsync(ResetPasswordRequestDto request);
+    Task ResetPasswordAsync(ResetPasswordDto request);
+    Task ChangePasswordAsync(Guid userId, ChangePasswordDto request);
 }
